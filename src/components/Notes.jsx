20531595ca3992d46b864e791e25f7096d122dd8
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-13 00:24:36 +0530

    Move addNote div to its own component AddNote and use in Notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/NoteContext";
+import AddNote from "./AddNote";
 import NoteItem from "./NoteItem";
 
 const Notes = () => {
-  const { notes, setNotes } = useContext(noteContext);
+  const { notes, addNote, updateNote, deleteNote } = useContext(noteContext);
   return (
-    <div className="row my-3">
-      <h2>Your Notes</h2>
-      {notes.map((note) => {
-        return <NoteItem key={note._id} note={note} />;
-      })}
-    </div>
+    <>
+      <AddNote />
+      <div className="container">
+        <div className="row my-3">
+          <h2>Your Notes</h2>
+          {notes.map((note) => {
+            return <NoteItem key={note._id} note={note} />;
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
