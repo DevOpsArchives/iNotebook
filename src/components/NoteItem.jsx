eedc3cb859db675a46dcd4f209f8586958af1f89
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-13 02:14:19 +0530

    Pass note details back to Parent Component

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -3,7 +3,7 @@ import noteContext from "../context/notes/NoteContext";
 
 const NoteItem = (props) => {
   const { deleteNote } = useContext(noteContext);
-  const { note } = props;
+  const { note, updateNote } = props;
 
   return (
     <div className="col-md-3">
@@ -11,6 +11,7 @@ const NoteItem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <p className="card-text">{note.tag}</p>
           <i
             className="far fa-trash-alt mx-2"
             style={{ cursor: "pointer" }}
@@ -21,6 +22,9 @@ const NoteItem = (props) => {
           <i
             className="far fa-edit my-1 float-end"
             style={{ cursor: "pointer" }}
+            onClick={() => {
+              updateNote(note);
+            }}
           ></i>
         </div>
       </div>
